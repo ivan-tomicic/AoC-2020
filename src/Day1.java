ff8ba470ca3c309fffabf076683b8f187e81018e
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-12-02 06:55:07 +0100

    solutons for day1 and day2

---
+++ src/Day1.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Day1 {
+
+    public static PrintWriter out;
+
+    public static void main(String[] args) {
+
+        List<Integer> input = new ArrayList<>();
+
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        while (true) {
+            try{
+                input.add(sc.nextInt());
+            } catch (NumberFormatException | NullPointerException exception) {
+                break;
+            }
+        }
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = i + 1; j < input.size(); j++) {
+                if (input.get(i) + input.get(j) == 2020) {
+                    System.out.println("Multiplication of 2 numbers: " + input.get(i) * input.get(j));
+                }
+                for (int z = j + 1; z < input.size(); z++) {
+                    if (input.get(i) + input.get(j) + input.get(z) == 2020) {
+                        System.out.println("Multiplication of 3 numbers: " + input.get(i) * input.get(j) * input.get(z));
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/Day2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+
+public class Day2 {
+
+    public static PrintWriter out;
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        int validPasswords1 = 0;
+        int validPasswords2 = 0;
+
+        while(true) {
+            try {
+                String line = sc.nextLine();
+                int left = Integer.parseInt(line.split("-")[0]);
+                int right = Integer.parseInt(line.split("-")[1].split("\\s+")[0]);
+                char letter =  line.split("\\s+")[1].split(":")[0].charAt(0);
+                String passwordLetters = line.split(":")[1].strip();
+
+                int counter = 0;
+
+                for(int i = 0; i < passwordLetters.length(); i++) {
+                    if(letter == passwordLetters.charAt(i)) counter++;
+                }
+                if(counter >= left && counter <= right) {
+                    validPasswords1++;
+                }
+                if(( passwordLetters.charAt(left - 1) == letter || passwordLetters.charAt(right - 1) == letter )
+                        && !( passwordLetters.charAt(left - 1) == letter && passwordLetters.charAt(right - 1) == letter )) {
+                    validPasswords2++;
+                }
+            } catch (Exception ex) {
+                System.out.println(validPasswords1);
+                System.out.println(validPasswords2);
+                break;
+            }
+        }
+    }
+
+}
