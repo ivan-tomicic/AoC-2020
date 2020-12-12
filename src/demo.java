4af797a0dcdc96f45bae2c7c6c15ecd8f2f29e37
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-12-12 11:23:31 +0100

    added day 12 solution

--- src/demo.java
+++
@@ -1,9 +0,0 @@
-public class demo {
-    public static void main(String[] args) {
-        String a = "R56";
-        System.out.println(a.split("[a-zA-Z]")[1]);
-        System.out.println(-1%5);
-        System.out.println(-13%4);
-        System.out.println(Math.sin((double)90/360*2*Math.PI));
-    }
-}
