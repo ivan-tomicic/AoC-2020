8b8f803f68eee1a60667809776189f3e034069d1
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-12-14 13:02:26 +0100

    added day 13 and 14 solutions

--- src/Day14.java
+++ src/Day14.java
@@ -1,56 +1,89 @@
 import java.util.*;
 
+
 public class Day14 {
-    static long[] numbers;
-    static Set<Integer> positions = new LinkedHashSet<>();
-    static Map<String, Map<Integer, String>> input;
+
+    static Map<Long, Long> memory;
+
+    static Map<String, Map<Long, Long>> input = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        numbers = new long[100000];
+
         String mask = sc.nextLine().split("\\s+")[2];
-        int position;
-        String value;
-        String result;
+        input.put(mask, new LinkedHashMap<>());
 
         while(sc.hasNextLine()) {
             String line = sc.nextLine();
             if(line.startsWith("mask")) {
                 mask = line.split("\\s+")[2];
+                input.put(mask, new LinkedHashMap<>());
             } else {
-                position = Integer.parseInt(line.split("\\[")[1].split("\\]")[0]);
-                positions.add(position);
+                input.get(mask).put(Long.parseLong(line.split("\\[")[1].split("\\]")[0]),
+                        Long.parseLong(line.split("\\s+")[2]));
+            }
+        }
 
-                StringBuilder sbValue = new StringBuilder(Long.toBinaryString(Long.parseLong(line.split("\\s+")[2])));
-                int leadingZeroes = sbValue.length();
-                for(int i = 0; i < 36 - leadingZeroes; i++) {
-                    sbValue.insert(0, "0");
-                }
-                value = sbValue.toString();
+        part1();
+        part2();
 
+    }
 
-                StringBuilder sbResult = new StringBuilder();
-                for(int i = 35; i >= 0; i--) {
-                    if(mask.charAt(i) == '0') sbResult.append("0");
-                    else if(mask.charAt(i) == '1') sbResult.append("1");
-                    else sbResult.append(value.charAt(i));
-                }
-                result = sbResult.toString();
+    public static void part1() {
 
+        memory = new LinkedHashMap<>();
 
+        for(String mask : input.keySet()) {
+            for(Long position : input.get(mask).keySet()) {
+                long value = input.get(mask).get(position);
                 long changedNumber = 0;
+
                 for(int i = 0; i < 36; i++) {
-                    changedNumber += Long.parseLong(Character.toString(result.charAt(i))) << i;
+                    if(mask.charAt(35 - i) == '1') changedNumber += (1L << i);
+                    else if(mask.charAt(35 - i) == 'X') changedNumber += value & (1L << i);
+                }
+                memory.put(position, changedNumber);
+            }
+        }
+
+        long sum = 0;
+        for(long value : memory.values()) sum += value;
+        System.out.println(sum);
+
+    }
+
+    public static void part2() {
+        memory = new LinkedHashMap<>();
+
+        for(String mask : input.keySet()) {
+            for(Long position : input.get(mask).keySet()) {
+                Set<Long> allPossiblePositions = new LinkedHashSet<>();
+
+                long changedPosition = position;
+                for(int i = 0; i < 36; i++) {
+                    if(mask.charAt(35 - i) == '1') changedPosition |= (1L << i);
+                    if(mask.charAt(35 - i) == 'X') changedPosition -= (changedPosition & (1L << i));
                 }
 
-                numbers[position] = changedNumber;
+                allPossiblePositions.add(changedPosition);
+                for(int i = 0; i < 36; i++) {
+                    if(mask.charAt(35 - i) == 'X') {
+                        Set<Long> temp = new LinkedHashSet<>();
+                        for(long pos : allPossiblePositions) {
+                            temp.add(pos);
+                            temp.add(pos | (1L << i));
+                        }
+                        allPossiblePositions.addAll(temp);
+                    }
+                }
+
+                for(long pos : allPossiblePositions) {
+                    memory.put(pos, input.get(mask).get(position));
+                }
             }
         }
         long sum = 0;
-        for(int pos : positions) {
-            sum += numbers[pos];
-            System.out.println("pos: " + pos + " number: " + numbers[pos]);
-        }
+        for(long value : memory.values()) sum += value;
         System.out.println(sum);
     }
 }
