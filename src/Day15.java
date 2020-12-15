01fa8f0b608e72badbc5073dc6eada8e994ba379
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-12-15 13:54:43 +0100

    edited day15 solution

--- src/Day15.java
+++ src/Day15.java
@@ -1,32 +1,29 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Day15 {
 
-    static Map<Integer, List<Integer>> numbers;
+    static int[][] numbers;
 
     public static void main(String[] args) {
 
-        numbers = new LinkedHashMap<>();
+        numbers = new int[30000000][2];
         Scanner sc = new Scanner(System.in);
         String[] input = sc.nextLine().split(",");
-        for(int i = 0; i < input.length; i++) {
-            numbers.put(Integer.parseInt(input[i]), Arrays.asList(i, i));
+        for (int i = 0; i < input.length; i++) {
+            numbers[Integer.parseInt(input[i])][0] = i + 1;
+            numbers[Integer.parseInt(input[i])][1] = i + 1;
         }
-        //for part1 splution replace 30000000 in for loop with 2020
-        //takes some time to run :/
+        //for part1 splution replace 30 000 000 in for loop with 2020
+        //takes about 10 seconds to run for 30 000 000:/
         int lastNumber = Integer.parseInt(input[input.length - 1]);
-        List<Integer> value;
-        for(int j = input.length; j < 30000000; j++) {
-            int key = numbers.get(lastNumber).get(0) - numbers.get(lastNumber).get(1);
-            if(numbers.containsKey(key)) value = Arrays.asList(j, numbers.get(key).get(0));
-            else value = Arrays.asList(j, j);
-            numbers.put(key, value);
+
+        for (int j = input.length; j < 30000000; j++) {
+            int key = numbers[lastNumber][0] - numbers[lastNumber][1];
+            if (numbers[key][1] != 0 && numbers[key][0] != 0) numbers[key][1] = numbers[key][0];
+            else numbers[key][1] = j + 1;
+            numbers[key][0] = j + 1;
             lastNumber = key;
         }
         System.out.println(lastNumber);
-
     }
-
-
 }
