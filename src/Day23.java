7558404c656833a9d45e28a0e2890178aefbcc4e
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-12-23 15:51:08 +0100

    added day 23 solution

--- src/Day23.java
+++ src/Day23.java
@@ -1,54 +1,69 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 public class Day23 {
 
-    static LinkedList<Integer> cups = new LinkedList<>();
+    static int[] cups;
 
     public static void main(String[] args) {
-
-
         //part 1;
-        cups.addAll(Arrays.asList(5, 8, 6, 4, 3, 9, 1, 7, 2));
-        playGame(9, 100);
+        cups = new int[10];
+        fillFirstEight();
+        cups[2] = 5;
+        playGame(100);
         System.out.print("Part 1 solution: ");
-        for (int i = cups.indexOf(1) + 1; i < 7 + cups.get(1); i++) System.out.print(cups.get(i % 9));
+        for(int i = cups[1], j = 0; j < 8; j++) {
+            System.out.print(i);
+            i = cups[i];
+        }
 
+        //part 2
+        cups = new int[1000001];
+        fillFirstEight();
+        cups[2] = 10;
+        for (int i = 10; i < 1000000; i++) {
+            cups[i] = i+1;
+        }
+        cups[1000000] = 5;
 
-        cups.removeAll(cups);
-        cups.addAll(Arrays.asList(5, 8, 6, 4, 3, 9, 1, 7, 2));
-        for (int i = 10; i <= 1000000; i++) cups.add(i);
-        playGame(1000000, 10000000);
-        System.out.print("\nPart 2 splution: ");
-        System.out.print(cups.get(cups.indexOf(1) + 1)* cups.get(cups.indexOf(1) + 2));
+        playGame(10000000);
+        System.out.print("\nPart 2 solution: ");
+        System.out.println((long)cups[1]*(long)cups[cups[1]]);
 
     }
 
-    static void playGame(int size, int moves) {
-        int currentCupIndex = 0;
-        int currentCup = cups.get(currentCupIndex);
+    static void playGame(int moves) {
+        int currentCup= 5;
 
         for (int i = 0; i < moves; i++) {
-            if(i%10000 == 0) System.out.println(i);
-            Integer removeCup3 = cups.get((currentCupIndex + 3) % size);
-            Integer removeCup2 = cups.get((currentCupIndex + 2) % size);
-            Integer removeCup1 = cups.get((currentCupIndex + 1) % size);
-
-            cups.remove(removeCup1);
-            cups.remove(removeCup2);
-            cups.remove(removeCup3);
+            int cup1 = cups[currentCup];
+            int cup2 = cups[cups[currentCup]];
+            int cup3 = cups[cups[cups[currentCup]]];
+            cups[currentCup] = cups[cups[cups[cups[currentCup]]]];
 
             int destination = currentCup - 1;
-            while (!cups.contains(destination) && destination > 0) destination--;
-            if (destination == 0) destination = cups.stream().max(Comparator.naturalOrder()).get();
-
-            int destionationIndex = cups.indexOf(destination);
-            cups.add((destionationIndex + 1)%size, removeCup1);
-            cups.add((destionationIndex + 2)%size, removeCup2);
-            cups.add((destionationIndex + 3)%size, removeCup3);
-
-            currentCupIndex = (cups.indexOf(currentCup) + 1) % size;
-            currentCup = cups.get(currentCupIndex);
-
+            while(destination > 0 && (destination == cup1 || destination == cup2 || destination == cup3)) destination--;
+            if(destination == 0) {
+                int[] copy = cups.clone();
+                int finalCurrentCup = currentCup;
+                destination = Arrays.stream(copy).filter(a -> a != cup1 && a!= cup2 && a!= cup3 && a!= finalCurrentCup).max().getAsInt();
+            }
+            int nextOfDestination = cups[destination];
+            cups[destination] = cup1;
+            cups[cup3] = nextOfDestination;
+            currentCup = cups[currentCup];
         }
+    }
+
+    static void fillFirstEight() {
+        cups[5] = 8;
+        cups[8] = 6;
+        cups[6] = 4;
+        cups[4] = 3;
+        cups[3] = 9;
+        cups[9] = 1;
+        cups[1] = 7;
+        cups[7] = 2;
     }
 }
